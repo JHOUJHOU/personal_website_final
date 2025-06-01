b2d03b7fcb207afc0e834b0808239559d220b151
jhoujhou <a01450887@gmail.com>
2025-06-02 00:13:39 +0800

    完成職涯諮詢成功案例

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import './assets/scss/all.scss';
+
+var swiper = new Swiper(".blog-mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      navigation: {
+        nextEl: ".swiper-blog-next",
+        prevEl: ".swiper-blog-prev",
+      },
+      grid: {
+        rows: 1,
+        fill: 'row',
+      }
+    }
+  }
+});
